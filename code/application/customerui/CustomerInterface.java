902258148073327258d007d78fb28825baf001ba
gohse <gohse@Sans_sean>
2024-04-20 03:19:21 +0800

    Javadoc comments Test run on "customer interface" class Changes by : Sean Date/Time : 20/4/2024 , 0312hrs

--- code/application/customerui/CustomerInterface.java
+++ code/application/customerui/CustomerInterface.java
@@ -6,7 +6,21 @@ import java.util.Scanner;
 import branch.Branch;
 import branch.OrderManagement;
 
+
+/**
+ * 
+ * @author Lam Do
+ * @version 1.1
+ * @since 2024-19-04
+ *
+ */
 public class CustomerInterface {
+	
+	/**
+	 * Constructor for Customer Interface
+	 * @param sc Scanner object 
+	 * @param branchList ArrayList of Branch objects
+	 */
     public CustomerInterface(Scanner sc, ArrayList<Branch> branchList) {
         System.out.println("--------------------");
         System.out.println("LOGIN AS CUSTOMER");
